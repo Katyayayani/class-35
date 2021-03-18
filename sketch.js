4e2325144e12c6cb48b68d1fa9954e037250d97a
Katyayayani <itskatyayani.singh@gmail.com>
2021-03-18 17:01:02 +0530

    realtime database

--- sketch.js
+++ sketch.js
@@ -1,29 +1,42 @@
-var ball;
+var hball,position,database;
 
 function setup(){
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+    database=firebase.database()
+    hball = createSprite(250,250,10,10);
+    hball.shapeColor = "red";
+
+    var hballPosition=database.ref('ball/position');
+    hballPosition.on("value",readPosition);
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    database.ref('ball/position').set({
+        'x':position.x+x,
+        'y':position.y+y
+    })
+    }
+
+function readPosition(data){
+    position=data.val();
+    hball.x=position.x
+    hball.y=position.y
+
 }
